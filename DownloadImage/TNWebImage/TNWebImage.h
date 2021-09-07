42179d3e51e4c1ed4c130b690b8b26e2f9e00273
Trieu Nguyen <trieu.nguyen@geocomply.com>
2021-09-07 23:23:19 +0700

    Update framework target

---
+++ DownloadImage/TNWebImage/TNWebImage.h
@@ -0,0 +1,18 @@
+//
+//  TNWebImage.h
+//  TNWebImage
+//
+//  Created by Trieu Nguyen on 03/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TNWebImage.
+FOUNDATION_EXPORT double TNWebImageVersionNumber;
+
+//! Project version string for TNWebImage.
+FOUNDATION_EXPORT const unsigned char TNWebImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TNWebImage/PublicHeader.h>
+
+#import <TNWebImage/UIImageView+TNWebImage.h>
